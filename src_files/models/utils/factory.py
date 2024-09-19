040b6cfa4d01d22146ce3e9d77c36221c1b5c3a2
daothuphuong98 <68388218+daothuphuong98@users.noreply.github.com>
2024-09-19 17:45:36 +0700

    Update factory.py

--- src_files/models/utils/factory.py
+++ src_files/models/utils/factory.py
@@ -56,3 +56,52 @@ def create_model(args,load_head=False):
             model.load_state_dict(state[key], strict=True)
 
     return model
+
+def create_model2(args,load_head=False):
+    """Create a model
+    """
+    model_params = {'args': args, 'num_classes': args.num_classes}
+    args = model_params['args']
+    args.model_name = args.model_name.lower()
+
+    if args.model_name == 'tresnet_m':
+        model = TResnetM(model_params)
+    elif args.model_name == 'tresnet_l':
+        model = TResnetL(model_params)
+    elif args.model_name == 'tresnet_xl':
+        model = TResnetXL(model_params)
+    else:
+        print("model: {} not found !!".format(args.model_name))
+        exit(-1)
+
+    ####################################################################################
+    if args.use_ml_decoder:
+        model = add_ml_decoder_head(model,num_classes=args.num_classes,num_of_groups=args.num_of_groups,
+                                    decoder_embedding=args.decoder_embedding, zsl=args.zsl)
+    ####################################################################################
+    # loading pretrain model
+    model_path = args.model_path
+    print(model_path)
+#     if args.model_name == 'tresnet_l' and os.path.exists("./tresnet_l.pth"):
+#         model_path = "./tresnet_l.pth"
+    if model_path:  # make sure to load pretrained model
+        if not os.path.exists(model_path):
+            print("downloading pretrain model...")
+            request.urlretrieve(args.model_path, "./tresnet_l.pth")
+            model_path = "./tresnet_l.pth"
+            print('done')
+        print('Loading model from', model_path)
+        state = torch.load(model_path, map_location='cpu')
+        if 'model' in state:
+            key = 'model'
+        else:
+            key = 'state_dict'
+        
+        if not load_head:
+            filtered_dict = {k: v for k, v in state[key].items() if
+                             (k in model.state_dict() and 'head.decoder' not in k)}
+            model.load_state_dict(filtered_dict, strict=False)
+        else:
+            model.load_state_dict(state, strict=True)
+
+    return model
